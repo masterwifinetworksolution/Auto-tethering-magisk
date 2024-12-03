5d4c36c6a663a0bdb152d9d38113802710872448
Master Wifi Network Solution <46000841+masterwifinetworksolution@users.noreply.github.com>
2024-12-03 20:25:08 +0800

    Update auto_tether.sh

--- auto_tether.sh
+++ auto_tether.sh
@@ -1,11 +1,11 @@
-#!/system/bin/sh
+#!/data/local/tmp/sh
 # Script untuk mengaktifkan tethering USB secara otomatis ketika USB terhubung
 
 # Path ke perintah untuk mengaktifkan tethering USB
-TETHERING_CMD="/system/bin/tethering enable"
+TETHERING_CMD="/data/local/tmp/tethering enable"
 
 # Path ke file log untuk mencatat aktivitas
-LOG_FILE="/system/log/auto_tether.log"
+LOG_FILE="/data/local/tmp/auto_tether.log"
 
 # Fungsi untuk mengaktifkan tethering USB
 enable_tethering() {
@@ -16,7 +16,7 @@ enable_tethering() {
 # Fungsi untuk menonaktifkan tethering USB
 disable_tethering() {
     echo "Menonaktifkan tethering USB..." >> $LOG_FILE
-    /system/bin/tethering disable
+    /data/local/tmp/tethering disable
 }
 
 # Cek apakah USB terhubung
